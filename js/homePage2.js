490105c4e28143b3f21b8b390b0eb4ea54067d9a
kyj <flysoar3029@gmail.com>
2022-07-13 20:15:52 +0900

    homePage2

---
+++ js/homePage2.js
@@ -0,0 +1,4 @@
+document.querySelector(".prev_slide").addEventListener("click", function () {
+  document.querySelector(".image_container").getElementsByClassName.transform =
+    "translate(-100vw)";
+});
